0360d0fff22dc5632af79e8119324dbcd5ea592a
TolMen <cotton.frachisse@gmail.com>
2023-08-23 09:38:07 +0200

    Folder and directory file creation

---
+++ VariousInfo/directoryFirstName.py
@@ -0,0 +1,8 @@
+
+listFirstName = [
+    "Alice", "Bob", "Claire", "David", "Emma", "Francis", "Grace",
+    "Henry", "Isabelle", "Jack", "Kate", "Liam", "Mia", "Noah",
+    "Olivia", "Paul", "Quinn", "Ryan", "Sophia", "Thomas", "Uma",
+    "Victor", "Willow", "Xander", "Yara", "Zachary", "Ava", "Benjamin",
+    "Chloe", "Daniel", "Emily"
+]
