fd9d1c78a0981e12f71ec3781fe3e06e330c4d6d
TolMen <cotton.frachisse@gmail.com>
2023-08-23 17:15:58 +0200

    Minor change

--- VariousInfo/directoryFirstName.py
+++ VariousInfo/directoryFirstName.py
@@ -1,8 +1,15 @@
 
 listFirstName = [
-    "Alice", "Bob", "Claire", "David", "Emma", "Francis", "Grace",
-    "Henry", "Isabelle", "Jack", "Kate", "Liam", "Mia", "Noah",
-    "Olivia", "Paul", "Quinn", "Ryan", "Sophia", "Thomas", "Uma",
-    "Victor", "Willow", "Xander", "Yara", "Zachary", "Ava", "Benjamin",
-    "Chloe", "Daniel", "Emily"
+    "Emma", "Olivia", "Ava", "Isabella", "Sophia", "Mia", "Charlotte", "Amelia", "Harper", "Evelyn",
+    "Liam", "Noah", "William", "James", "Oliver", "Benjamin", "Elijah", "Lucas", "Mason", "Logan",
+    "Alexander", "Ethan", "Jacob", "Michael", "Daniel", "Matthew", "Henry", "Joseph", "Samuel", "David",
+    "Andrew", "John", "Nicholas", "Anthony", "Joshua", "Christopher", "Gabriel", "Andrew", "William", "Ryan",
+    "David", "Daniel", "Paul", "James", "Jason", "Matthew", "Brian", "William", "Jeffrey", "Eric",
+    "Emily", "Abigail", "Harper", "Emma", "Olivia", "Isabella", "Sophia", "Ava", "Mia", "Amelia",
+    "Elizabeth", "Samantha", "Ella", "Grace", "Lily", "Chloe", "Hannah", "Avery", "Evelyn", "Leah",
+    "Liam", "Noah", "Ethan", "Mason", "Logan", "Lucas", "Alexander", "Oliver", "Daniel", "Michael",
+    "Matthew", "David", "Joseph", "Elijah", "Benjamin", "James", "William", "Andrew", "Gabriel", "Samuel",
+    "Christopher", "John", "Nicholas", "Anthony", "Ryan", "Joshua", "Daniel", "David", "James", "Matthew",
+    "Jason", "Joseph", "William", "Brian", "Robert", "John", "Michael", "Charles", "Richard", "David",
+    "James", "William", "John", "Robert", "Joseph", "Christopher", "Daniel", "Richard", "David", "Charles"
 ]
